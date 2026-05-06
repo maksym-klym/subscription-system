4cb15b82112e9cb8f1dbd0f7dca2222bcca07ee4
Maksym Klym <maksym.klym.dev@gmail.com>
2026-05-06 14:43:16 +0200

    feat: add Subscription entity

---
+++ src/main/java/com/maks/subscriptionsystem/entity/Subscription.java
@@ -0,0 +1,41 @@
+package com.maks.subscriptionsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = {"user", "plan"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "subscriptions")
+@Entity
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "plan_id", nullable = false)
+    private Plan plan;
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SubscriptionStatus status;
+
+    public enum SubscriptionStatus{
+        TRIAL,
+        ACTIVE,
+        PAST_DUE,
+        CANCELED
+    }
+}
