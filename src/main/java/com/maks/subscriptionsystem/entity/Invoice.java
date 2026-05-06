385e064d8ae94350e3c4871f4b5dd2022d263185
Maksym Klym <maksym.klym.dev@gmail.com>
2026-05-06 14:56:48 +0200

    feat: add Invoice entity

---
+++ src/main/java/com/maks/subscriptionsystem/entity/Invoice.java
@@ -0,0 +1,38 @@
+package com.maks.subscriptionsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = "subscription")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "invoices")
+@Entity
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "subscription_id", nullable = false)
+    private Subscription subscription;
+    @Column(nullable = false)
+    private BigDecimal amount;
+    @Column(nullable = false)
+    private LocalDateTime dueDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private InvoiceStatus status;
+
+    public enum InvoiceStatus{
+        CREATED,
+        PAID,
+        OVERDUE
+    }
+}
